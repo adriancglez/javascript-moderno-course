f53e5a382bd1b71f2a0f25aacdeca7f3d04ab5c9
Adrian Gonzalez <yo.robot.1990@gmail.com>
2020-08-25 12:40:04 -0500

    Variables en JS - resolve #1

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,16 @@
+alert('Hola Mundo desde Archivo Externo');
+
+const nombre = prompt('¿Cuál es nombre?');
+
+// Toma el nombre y lo muestra en pantalla
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript Moderno`; /* document.querySelector('.contenido') Esto nos permite seleccionar un elemento del HTML */
+
+// Comentarios
+
+// Comentario de una línea
+/* Comentario
+de múltiples 
+líneas */
+
+// Asigna el valor hacia la variable producto
+const producto = 'Monitor de 24 pulgadas'
---
+++ 02-Variables/js/03-app.js
@@ -0,0 +1,7 @@
+// Deben inicializarse al mismo tiempo que se declaran
+const productoNuevo = 'Tablet';
+
+// Las variables declaradas const no se pueden reasignar
+productoNuevo = 'Monitor';
+
+console.log(productoNuevo);
